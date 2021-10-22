01e6c930dceddfffd77f361a24db66199a86d470
HYO0 <hyo02222@gmail.com>
2021-10-22 14:34:03 +0900

    login layout done

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,15 @@
 import React from "react";
 import "./App.css";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import Main from "./pages/Main";
 
 function App() {
-  return <div>Hello :)</div>;
+  return (
+    <Router>
+      <Route exact path="/" component={Main} />
+      <div>Hello :)</div>
+    </Router>
+  );
 }
 
 export default App;
