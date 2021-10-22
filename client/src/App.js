0772d73a2876edb2daa77c8c37c16ad743dea1fe
HYO0 <hyo02222@gmail.com>
2021-10-22 13:24:34 +0900

    app modified

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
+import React from "react";
 import "./App.css";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 function App() {
   return <div>Hello :)</div>;
--- client/src/components/Login/Login.js
+++ client/src/components/Login/Login.js
@@ -3,7 +3,25 @@ import React from "react";
 const Login = () => {
   return (
     <Container>
-      <FormWrap></FormWrap>
+      <FormWrap>
+        <Icon to="/">Mission Eco</Icon>
+        <FormContent>
+          <Form action="#">
+            <FormH1>Sign in to your account</FormH1>
+            <FormLabel htmlFor="for">Email</FormLabel>
+            <FormInput type="email" required />
+            <FormLabel htmlFor="for">Pssword</FormLabel>
+            <FormInput type="password" required />
+            <Text>이메일 또는 비밀번호를 확인해주세요.</Text>
+            <FormButton type="submit">입장하기!</FormButton>
+            <FormBtnBox>
+              <OauthBtn>구글로그인</OauthBtn>
+              <OauthBtn>카카오로그인</OauthBtn>
+            </FormBtnBox>
+            <FormButton type="submit">회원가입</FormButton>
+          </FormBtnBox>
+        </FormContent>
+      </FormWrap>
     </Container>
   );
 };
