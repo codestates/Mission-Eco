11e9929c8891b5306344787ec8b2f7cf420b75d3
eden0514 <ghworlas@naver.com>
2021-10-24 00:05:41 +0900

    server merge2

--- server/controllers/user/signUp.js
+++ server/controllers/user/signUp.js
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
-const { user } = require('../../models'); // 이름과 model폴더 디렉토리 확인
-const bcrypt = require('bcrypt');
-// 
-=======
 const { user } = require("../../models"); // 이름과 model폴더 디렉토리 확인
 const bcrypt = require("bcrypt");
 
->>>>>>> 56c9c4a785b6c3ab41bc5ccfa6b0075b7d84a2a2
 module.exports = {
   signUp: async (req, res) => {
     // 1. body값에서 email, nickname, password 구조분해 할당으로 받기
@@ -21,16 +15,6 @@ module.exports = {
 
       // DB에 동일한 이메일이 존재할 때
       const db_email = await user.findOne({ email });
-<<<<<<< HEAD
-      if(db_email) {
-        return res.status(409).send({ message: `${email} already exists.` });
-      }
-      console.log('signUp db_email', db_email)
-
-      // DB에 동일한 닉네임이 존재할 때
-      const db_nickname = await user.findOne({ nick_name: nickname });
-      if(db_nickname) {
-=======
       if (db_email) {
         return res.status(409).send({ message: `${email} already exists.` });
       }
@@ -38,10 +22,9 @@ module.exports = {
       // DB에 동일한 닉네임이 존재할 때
       const db_nickname = await user.findOne({ nick_name: nickname });
       if (db_nickname) {
->>>>>>> 56c9c4a785b6c3ab41bc5ccfa6b0075b7d84a2a2
         return res.status(409).send({ message: `${nickname} already exists.` });
       }
-      console.log('signUp db_nickname', db_nickname);
+      console.log("signUp db_nickname", db_nickname);
 
       // email, nickname, password 셋 중에 한 개라도 입력되지 않았을 때
       if (!email || !nickname || !password) {
@@ -57,21 +40,12 @@ module.exports = {
 
         // DB에 hash를 포함한 유저 정보를 저장한다.
         const new_user = await user.create({
-<<<<<<< HEAD
-                                nick_name: nickname,
-                                email,
-                                password: hash,
-                                point: 0
-                          });
-        console.log('signup new_user: ', new_user)
-=======
           nick_name: nickname,
           email,
           password: hash,
           point: 0,
         });
         console.log("signup new_user: ", new_user);
->>>>>>> 56c9c4a785b6c3ab41bc5ccfa6b0075b7d84a2a2
       });
 
       // 회원가입 성공
