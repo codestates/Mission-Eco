a851dd6f8f19f79f22aa43aa98a7effa73bdb0e4
eden0514 <ghworlas@naver.com>
2021-11-01 23:17:36 +0900

    error changed

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -1,19 +1,11 @@
 import React, { useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useHistory } from "react-router-dom";
-<<<<<<< HEAD
-import { getUserInfo, userSignin } from "../../Redux/actions";
-import axios from "axios";
-import { validEmail } from "../../utils/validation";
-import kakao from "../../imges/kakao.png";
-import Google from "../google/Google";
-=======
 import { userSignin, authSuccess } from "../../Redux/actions";
 import axios from "axios";
 import { validEmail } from "../../utils/validation";
 import kakaoLogo from "../../imges/kakao-logo.png";
 import googleLogo from "../../imges/google-logo.png";
->>>>>>> c4da12449afa44ba7659ddb200ca0f00135dffe9
 import {
   Container,
   LeftTxt,
@@ -33,14 +25,8 @@ import {
   LinkFindIdPwd,
   GeneralLogin,
   Text,
-<<<<<<< HEAD
-  //OauthBtn,
-  KakaoBtn,
-  Img,
-=======
   OauthBtn,
-  Logo
->>>>>>> c4da12449afa44ba7659ddb200ca0f00135dffe9
+  Logo,
 } from "./LoginStyle";
 
 axios.defaults.withCredentials = true;
@@ -108,52 +94,32 @@ function Login() {
   };
 
   const kakaoLogin = async () => {
-<<<<<<< HEAD
-    window.location.href = `${process.env.REACT_APP_API_URL}/auth/kakao`;
-    //어떻게 handlesuccess로 연결할 것인가
-  };
-
-=======
     //ㄷㅏ른 url로 이동
     window.location.href = `${process.env.REACT_APP_API_URL}/auth/kakao`;
   };
 
   const googleLogin = async () => {
     window.location.href = `${process.env.REACT_APP_API_URL}/auth/google`;
-  }
+  };
 
->>>>>>> c4da12449afa44ba7659ddb200ca0f00135dffe9
   return (
     <Container>
       {/* <FormH1>로그인</FormH1> */}
       <LeftTxt>Hello,</LeftTxt>
-      <RightTxt>We always<br/>think about<br/>our<br/>Earth</RightTxt>
+      <RightTxt>
+        We always
+        <br />
+        think about
+        <br />
+        our
+        <br />
+        Earth
+      </RightTxt>
       <FormWrap>
         <Icon to="/"></Icon>
         <FormContent>
           <Form onSubmit={(e) => e.preventDefault()}>
-<<<<<<< HEAD
-            <FormH1>Sign in to your account</FormH1>
-            <FormLabel htmlFor="for">Email</FormLabel>
-            <FormInput type="email" onChange={handleInputValue("email")} />
-            <FormLabel htmlFor="for">Pssword</FormLabel>
-            <FormInput
-              type="password"
-              onChange={handleInputValue("password")}
-            />
-            <Text>{errMsg}</Text>
-            <FormButton type="submit" onClick={loginRequestHandler}>
-              입장하기!
-            </FormButton>
-            <Google />
-            <KakaoBtn onClick={kakaoLogin}>
-              <Img src={kakao} />
-            </KakaoBtn>
-            <FormButton type="submit">
-              <BtnLink to="/signup">회원가입</BtnLink>
-            </FormButton>
-=======
-          <GeneralLogin>
+            <GeneralLogin>
               <div>
                 <FormLabel htmlFor="for">이메일</FormLabel>
                 <FormInput type="email" onChange={handleInputValue("email")} />
@@ -169,18 +135,21 @@ function Login() {
               <FormButton type="submit" onClick={loginRequestHandler}>
                 로그인
               </FormButton>
-            <BtnLink to="/"><LinkFindIdPwd>아이디 / 비밀번호 찾기</LinkFindIdPwd></BtnLink>
-            <BtnLink to="/signup"><LinkSignUp>회원가입</LinkSignUp></BtnLink>
+              <BtnLink to="/">
+                <LinkFindIdPwd>아이디 / 비밀번호 찾기</LinkFindIdPwd>
+              </BtnLink>
+              <BtnLink to="/signup">
+                <LinkSignUp>회원가입</LinkSignUp>
+              </BtnLink>
             </GeneralLogin>
             <SocialLoigin>
               <OauthBtn onClick={kakaoLogin}>
-                <Logo src={kakaoLogo}/>
+                <Logo src={kakaoLogo} />
               </OauthBtn>
               <OauthBtn onClick={googleLogin}>
-                <Logo src={googleLogo}/>
+                <Logo src={googleLogo} />
               </OauthBtn>
             </SocialLoigin>
->>>>>>> c4da12449afa44ba7659ddb200ca0f00135dffe9
           </Form>
         </FormContent>
       </FormWrap>
