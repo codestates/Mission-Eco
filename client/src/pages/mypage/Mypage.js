444fc4a8715d4dafbfc1033b452a597e5c9976fd
cho-hyeonjin <gusls2005@gmail.com>
2021-11-08 11:24:48 +0900

    Mypage-mediaQuery edit

--- client/src/pages/mypage/Mypage.js
+++ client/src/pages/mypage/Mypage.js
@@ -1,9 +1,79 @@
+// import React, { useState, useEffect } from "react";
+// import {
+//   H1,
+//   H2,
+//   H3,
+//   MypageBtn,
+//   BtnLink,
+//   LinkCheckPassword,
+//   MypageContainer,
+//   Container,
+//   Title,
+//   ListContainer,
+//   ListItem,
+// } from "./MypageStyle";
+// import LikeList from "../../components/MyInfo/MypageLIke/LikeList/LikeList";
+// import MyLogList from "../../components/MyInfo/MypageLIke/MyLogList/MyLogList";
+// // import blob1 from "../../imges/blob1.svg";
+// import Footer from "../../components/Footer/Footer";
+
+// const Mypage = () => {
+//   useEffect(() => {}, []);
+
+//   return (
+//     <>
+//       <MypageContainer>
+//         <H1>My Page</H1>
+//         {/* mypage-top-container ( 페이지 타이틀, 마이페이지 수정버튼 ) */}
+//         <MypageBtn>
+//           <BtnLink to="/checkpassword">
+//             <LinkCheckPassword>&nbsp; 수정하기 ✍🏻</LinkCheckPassword>
+//           </BtnLink>
+//         </MypageBtn>
+
+//         {/* mypage-1st-container (유저 닉네임, 이 유저가 획득한 뱃지 갯수) */}
+//         <Container>
+//           <Title>닉네임: 환경요정</Title>
+//           <Title>에코뱃지: 7개</Title>
+//         </Container>
+
+//         {/* mypage-2nd-container ( 이 유저가 획득한 뱃지 이미지들 ) */}
+//         <Container>
+//           <H2>나의 에코뱃지</H2>
+//           <ListContainer>
+//             <ListItem>뱃지 리스트 들어올 예정</ListItem>
+//           </ListContainer>
+//         </Container>
+
+//         {/* // * ================================================================================ */}
+//         {/* res에서 challengeLikeList만 솎아서 */}
+//         <Container>
+//           <H2>내가 Like한 챌린지 List</H2>
+//           <ListContainer>
+//             <LikeList />
+//           </ListContainer>
+//         </Container>
+
+//         <Container>
+//           <H2>내가 작성한 챌린지Log List</H2>
+//           <ListContainer>
+//             <MyLogList />
+//           </ListContainer>
+//         </Container>
+//         <Footer />
+//       </MypageContainer>
+//     </>
+//   );
+// };
+
+// export default Mypage;
+
 import React, { useState, useEffect } from "react";
-import Navbar from "../../components/Navbar/Navbar";
 import {
   H1,
   H2,
   H3,
+  MypageWrapper,
   MypageBtn,
   BtnLink,
   LinkCheckPassword,
@@ -24,44 +94,45 @@ const Mypage = () => {
   return (
     <>
       <MypageContainer>
-        <H1>My Page</H1>
-        {/* mypage-top-container ( 페이지 타이틀, 마이페이지 수정버튼 ) */}
-        <MypageBtn>
-          <BtnLink to="/checkpassword">
-            <LinkCheckPassword>&nbsp; 수정하기 ✍🏻</LinkCheckPassword>
-          </BtnLink>
-        </MypageBtn>
+        <MypageWrapper>
+          <H1>My Page</H1>
+          {/* mypage-top-container ( 페이지 타이틀, 마이페이지 수정버튼 ) */}
+          <MypageBtn>
+            <BtnLink to="/checkpassword">
+              <LinkCheckPassword>수정하기 ✍🏻</LinkCheckPassword>
+            </BtnLink>
+          </MypageBtn>
 
-        {/* mypage-1st-container (유저 닉네임, 이 유저가 획득한 뱃지 갯수) */}
-        <Container>
-          <Title>닉네임: 환경요정</Title>
-          <Title>에코뱃지: 7개</Title>
-        </Container>
+          {/* mypage-1st-container (유저 닉네임, 이 유저가 획득한 뱃지 갯수) */}
+          <Container>
+            <Title>닉네임: 환경요정</Title>
+            <Title>에코뱃지: 7개</Title>
+          </Container>
 
-        {/* mypage-2nd-container ( 이 유저가 획득한 뱃지 이미지들 ) */}
-        <Container>
-          <H2>나의 에코뱃지</H2>
-          <ListContainer>
-            <ListItem>뱃지 리스트 들어올 예정</ListItem>
-          </ListContainer>
-        </Container>
+          {/* mypage-2nd-container ( 이 유저가 획득한 뱃지 이미지들 ) */}
+          <Container>
+            <H2>나의 에코뱃지</H2>
+            <ListContainer>
+              <ListItem>뱃지 리스트 들어올 예정</ListItem>
+            </ListContainer>
+          </Container>
 
-        {/* // * ================================================================================ */}
-        {/* res에서 challengeLikeList만 솎아서 */}
-        <Container>
-          <H2>내가 Like한 챌린지 List</H2>
-          <ListContainer>
-            <LikeList />
-          </ListContainer>
-        </Container>
+          {/* // * ================================================================================ */}
+          {/* res에서 challengeLikeList만 솎아서 */}
+          <Container>
+            <H2>내가 Like한 챌린지 List</H2>
+            <ListContainer>
+              <LikeList />
+            </ListContainer>
+          </Container>
 
-        <Container>
-          <H2>내가 작성한 챌린지Log List</H2>
-          <ListContainer>
-            <MyLogList />
-          </ListContainer>
-        </Container>
-        <Footer />
+          <Container>
+            <H2>내가 작성한 챌린지Log List</H2>
+            <ListContainer>
+              <MyLogList />
+            </ListContainer>
+          </Container>
+        </MypageWrapper>
       </MypageContainer>
     </>
   );
--- client/src/pages/mypage/MypageStyle.js
+++ client/src/pages/mypage/MypageStyle.js
@@ -1,13 +1,127 @@
+// import styled from "styled-components";
+// import { Link } from "react-router-dom";
+// import blob1 from "../../imges/blob1.svg";
+
+// export const MypageContainer = styled.div`
+//   /* margin-top: 15em; */
+//   padding-top: 10em;
+
+//   @media screen and (max-width: 768px) {
+//   }
+//   @media screen and (max-width: 480px) {
+//   }
+
+//   background-image: url(${blob1});
+//   height: 100%;
+//   background-position: center;
+//   background-size: cover;
+//   -webkit-background-size: cover;
+//   -moz-background-size: cover;
+//   -o-background-size: cover;
+//   background-size: cover;
+//   position: relative;
+//   background-attachment: fixed;
+// `;
+
+// export const H1 = styled.h1`
+//   display: flex;
+//   justify-content: center;
+//   margin-bottom: 2em;
+//   font-size: 2.5em;
+// `;
+
+// export const H2 = styled.h2`
+//   display: flex;
+// `;
+
+// export const H3 = styled.h3``;
+
+// export const MypageBtn = styled.div`
+//   display: flex;
+//   justify-content: right;
+//   align-items: center;
+//   margin: 2em;
+//   @media screen and (max-width: 768px) {
+//     width: 600px;
+//   }
+//   @media screen and (max-width: 480px) {
+//     width: 300;
+//   }
+// `;
+
+// export const BtnLink = styled(Link)`
+//   text-decoration: none;
+// `;
+// export const LinkCheckPassword = styled.div`
+//   border: solid #5d8767;
+//   border-radius: 50px;
+//   background: #5d8767;
+//   white-space: nowrap;
+//   padding: 5px 15px;
+//   color: #fff;
+//   font-size: 15px;
+//   font-weight: 500;
+//   outline: none;
+//   cursor: pointer;
+//   transition: all 0.2s ease-in-out;
+//   text-decoration: none;
+
+//   &:hover {
+//     transition: all 0.2s ease-in-out;
+//     background: #fff;
+//     color: #5d8767;
+//     font-size: 15px;
+//     font-weight: 600;
+//     border: solid #5d8767;
+//   }
+// `;
+
+// export const Container = styled.div`
+//   background: whitesmoke;
+//   margin: 15px;
+//   padding: 20px;
+//   border: 3px solid gray;
+//   border-radius: 10px;
+// `;
+
+// export const Title = styled.p`
+//   @media screen and (max-width: 768px) {
+//   }
+//   @media screen and (max-width: 480px) {
+//   }
+// `;
+
+// export const ListContainer = styled.ul`
+//   /* background: brown; */
+//   display: flex;
+//   flex-direction: row;
+//   padding: 20px;
+
+//   @media screen and (max-width: 768px) {
+//   }
+//   @media screen and (max-width: 480px) {
+//   }
+// `;
+
+// export const ListItem = styled.li`
+//   /* background: midnightblue; */
+//   @media screen and (max-width: 768px) {
+//   }
+//   @media screen and (max-width: 480px) {
+//   }
+// `;
+
 import styled from "styled-components";
 import { Link } from "react-router-dom";
 import blob1 from "../../imges/blob1.svg";
 
 export const MypageContainer = styled.div`
   /* margin-top: 15em; */
-  padding-top: 10em;
+  //padding-top: 10em;
 
   @media screen and (max-width: 768px) {
   }
+
   @media screen and (max-width: 480px) {
   }
 
@@ -23,11 +137,28 @@ export const MypageContainer = styled.div`
   background-attachment: fixed;
 `;
 
+export const MypageWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 1000px;
+  margin: 0 auto;
+
+  z-index: -1;
+`;
+
 export const H1 = styled.h1`
   display: flex;
+  width: 100%;
+  margin-top: 4em;
   justify-content: center;
-  margin-bottom: 2em;
+  margin-bottom: 0.5em;
   font-size: 2.5em;
+  @media screen and (max-width: 768px) {
+  }
+  @media screen and (max-width: 480px) {
+  }
 `;
 
 export const H2 = styled.h2`
@@ -57,9 +188,9 @@ export const LinkCheckPassword = styled.div`
   border-radius: 50px;
   background: #5d8767;
   white-space: nowrap;
-  padding: 5px 20px;
+  padding: 5px 17px;
   color: #fff;
-  font-size: 18px;
+  font-size: 15px;
   font-weight: 500;
   outline: none;
   cursor: pointer;
@@ -70,7 +201,7 @@ export const LinkCheckPassword = styled.div`
     transition: all 0.2s ease-in-out;
     background: #fff;
     color: #5d8767;
-    font-size: 18px;
+    font-size: 15px;
     font-weight: 600;
     border: solid #5d8767;
   }
@@ -78,10 +209,12 @@ export const LinkCheckPassword = styled.div`
 
 export const Container = styled.div`
   background: whitesmoke;
+  width: 100%;
   margin: 15px;
   padding: 20px;
   border: 3px solid gray;
   border-radius: 10px;
+  overflow: hidden;
 `;
 
 export const Title = styled.p`
@@ -92,7 +225,6 @@ export const Title = styled.p`
 `;
 
 export const ListContainer = styled.ul`
-  /* background: brown; */
   display: flex;
   flex-direction: row;
   padding: 20px;
@@ -104,7 +236,6 @@ export const ListContainer = styled.ul`
 `;
 
 export const ListItem = styled.li`
-  /* background: midnightblue; */
   @media screen and (max-width: 768px) {
   }
   @media screen and (max-width: 480px) {
