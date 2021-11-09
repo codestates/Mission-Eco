585cfe62d4feb6bbe528377998f556ebaf17b1f0
loocia <loocia1910@gmail.com>
2021-11-09 22:08:08 +0900

    Hide console.log

--- client/src/components/MyInfo/Mybadge/MybadgeList.js
+++ client/src/components/MyInfo/Mybadge/MybadgeList.js
@@ -13,15 +13,15 @@ function MybadgeList() {
     
     // 전체 뱃지 리스트
     const [ badgeLists, setBadgeLists ] = useState([]);
-    console.log('전체 뱃지 리스트 badgeLists', badgeLists)
+    //console.log('전체 뱃지 리스트 badgeLists', badgeLists)
 
     // 유저 뱃지 리스트
     const [ myBadgeList, setMyBadgeList ] = useState([]);
-    console.log('유저 뱃지 리스트 myBadgeList', myBadgeList)
+    //console.log('유저 뱃지 리스트 myBadgeList', myBadgeList)
     
     // 유저가 획득한 뱃지들의 아이디만 축출
     const myBadgeIdList = myBadgeList.map(bg => bg.badge_id);
-    console.log(myBadgeIdList, 'myBadgeIdListmyBadgeIdList')
+    //console.log(myBadgeIdList, 'myBadgeIdListmyBadgeIdList')
     
     // 회원가입시 제공 뱃지(나는야 에코새싹왕, 뱃지아이디 1)는 기본값으로 저장
     myBadgeIdList.push(1);
