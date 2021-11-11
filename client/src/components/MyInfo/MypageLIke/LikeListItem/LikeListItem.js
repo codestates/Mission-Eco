00e4571302fb85bd32220024c5692d680109e09b
cho-hyeonjin <gusls2005@gmail.com>
2021-11-12 01:33:47 +0900

    mypage likeList err solving

--- client/src/components/MyInfo/MypageLIke/LikeListItem/LikeListItem.js
+++ client/src/components/MyInfo/MypageLIke/LikeListItem/LikeListItem.js
@@ -1,6 +1,3 @@
-// 전체 좋아요 받아오는 틀. ChallengeLogItem 참고
-// 여기서 맵핑 돌려서 LikeList로
-// ! LikeListItem용으로 수정해야함 (style.js도)
 import React from "react";
 import {
   CardContainer,
@@ -8,16 +5,14 @@ import {
   CardInner,
   ChallengeImg,
   LikeConatainer,
-} from "../../.././Challenge/ChallengeListItem/ChallengeListItemStyle";
+} from "../../MypageLIke/LikeListItem/LikeListItemStyle";
 
 const LikeListItem = ({ list }) => {
   return (
     <>
-      {/* // * 여기가 문제 */}
       <CardContainer>
         <CardInner>
           <ChallengeCard background={list.img}>
-            {/* //* 요 위에까지가 문제. ChallengeImg 사이즈랑 일치시켜라 */}
             <ChallengeImg src={list.img} />
             <LikeConatainer></LikeConatainer>
           </ChallengeCard>
