844f1ab05dc3a6621d56ca893d03d710c22c0b52
eden0514 <ghworlas@naver.com>
2021-11-11 09:58:07 +0900

    detail change

--- server/controllers/challenge/index.js
+++ server/controllers/challenge/index.js
@@ -1,6 +1,5 @@
 module.exports = {
   showChallenge: require("./showChallenge"),
-  likeList: require("./likeList").likeList,
   userLike: require("./likeList").userLike,
   like: require("./likeUnlike").like,
   unLike: require("./likeUnlike").unLike,
