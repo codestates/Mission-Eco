fcf955f24021c12e427b4e665e5e1ea51b62b919
HYO0 <hyo02222@gmail.com>
2021-11-11 16:14:40 +0900

    main upload section

--- client/src/pages/Main.js
+++ client/src/pages/Main.js
@@ -1,24 +1,18 @@
+/*eslint-disable */
 import React from "react";
 import HeroSection from "../components/HeroSection/HeroSection";
 import MainInfo from "../components/InfoSection/MainInfo";
 import { ScrollTopBtn } from "../components/ScrollTop/ScrollTopBtn";
-import {
-  homeObjOne,
-  homeObjTwo,
-  homeObjThree,
-  homeObjFour,
-} from "../components/InfoSection/Data";
+import { homeObjTwo } from "../components/InfoSection/Data";
 import MainInfo2 from "../components/InfoSection/MainInfo2";
 
 function Main() {
   return (
     <div>
       <HeroSection />
-      <MainInfo {...homeObjOne} />
 
       <MainInfo2 {...homeObjTwo} />
-      <MainInfo {...homeObjThree} />
-      <MainInfo {...homeObjFour} />
+
       <ScrollTopBtn />
     </div>
   );
